880daefa6c1c33462a0555ff1819f6d9b0733fa9
MATHEUS <matheusoliveira_ss97@live.com>
2019-08-02 02:06:51 -0300

    For

--- PrimeiroGit/Program.cs
+++ PrimeiroGit/Program.cs
@@ -11,6 +11,11 @@ namespace PrimeiroGit
             Console.ReadLine();
 
             Console.WriteLine("A");
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("OI");   
+            }
         }
     }
 }
